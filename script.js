9f95927f364efb7c399560b04ee65882cb09c931
matthew <127mwilliams@frhsd.com>
2025-12-15 14:03:02 -0500

    fdff

---
+++ script.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("generateBtn");
+button.addEventListener('click', getWords);
+
+
+function getWords(){
+    console.log(words);
+
+    
+}
+
+var words = document.querySelectorAll('input[type=text]')
+
