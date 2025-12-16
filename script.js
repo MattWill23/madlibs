655509da1d987c63b5c55d5d7900e0baf0ec8ac9
matthew <127mwilliams@frhsd.com>
2025-12-16 14:04:24 -0500

    dff

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var button = document.getElementById("generateBtn");
-button.addEventListener('click', getWords);
 
-
-function getWords(){
+var button = document.getElementById('generateBtn');
+button.addEventListener("click", getwords);
+function getwords() {
+    var words = document.querySelectorAll("input[type=text]")
+    var placeholders = document.querySelectorAll(".userInput")
     console.log(words);
 
+    placeholders.forEach((placeholder, index) => {
+        console.log(placeholder, index);
+<span class="userInput">
     
+</span>
+    })<
 }
-
-var words = document.querySelectorAll('input[type=text]')
-
