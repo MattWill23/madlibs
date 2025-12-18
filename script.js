b2afd83e1abb628d628ca4d407d9d51a9fa76319
matthew <127mwilliams@frhsd.com>
2025-12-18 12:22:10 -0500

    vxvx

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ var button = document.getElementById('generateBtn');
 button.addEventListener("click", getwords);
 function getwords() {
     var words = document.querySelectorAll("input[type=text]")
-    var placeholders = document.querySelectorAll(".userInput")
+    var placeholders = document.querySelectorAll(".user-input")
     console.log(words);
 
     placeholders.forEach((placeholder, index) => {
         console.log(placeholder, index);
-<span class="userInput">
-    
-</span>
-    })<
+        placeholder.textContent = words[index].value;
+    })
+    document.getElementById('story').style.display = 'block';
 }
